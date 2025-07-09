95e49ae539364e033dc81b3f296b0062c137f014
xinmei <yaowenchang3@gmail.com>
2025-07-09 23:23:40 +0800

    modify database.py into module folder

--- module/database.py
+++ module/database.py
@@ -56,7 +56,7 @@ class DataBase:
     def insert_tiktok(self, data):
         sql = "INSERT INTO tiktok (昵称, 抖音号, 粉丝数, 关注数, 获赞数, 作品数, 喜欢作品数, 签名, 时间) VALUES " + "(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
         self.cursor.execute(sql, data)
-        self.connection.commit()  # 提交事务
+        self.connection.commit()
     
     def insert_tiktok_video(self,data):
         sql = "INSERT INTO tiktok_video (视频_id, 标题, 评论数, 点赞数, 分享数, 创建时间, 视频地址) VALUES " + "(%s, %s, %s, %s, %s, %s, %s)"
@@ -66,7 +66,7 @@ class DataBase:
     def insert_weibo(self, data):
         sql = "INSERT INTO weibo (昵称, 简介, 转评赞, 累计评论量, 累计获赞, 粉丝数, 关注数, 微博数, 时间) VALUES " + "(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
         self.cursor.execute(sql, data)
-        self.connection.commit() # 提交事务
+        self.connection.commit()
 
     def insert_redbook(self, data):
         sql = "INSERT INTO red (小红书号, 昵称, IP地址, 简介, 作品数, 关注数, 粉丝数, 喜欢作品数, 时间) VALUES " + "(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
@@ -75,7 +75,7 @@ class DataBase:
 
 
 if __name__ == "__main__":
-        # 测试代码
+    # 测试代码
     database = DataBase()  # 创建数据库连接实例
 
     last_row, last_row_dict = database.search_tiktok()  # 获取最后一行数据
@@ -83,6 +83,6 @@ if __name__ == "__main__":
     print(last_row)
     print(last_row_dict)
     # tuple_data = ('118', '46', '75', '66', '105', '81', 'example_content')  # 示例数据
-    # database.insert_tiktok(tuple_data)  # 插入数据
-    # database.insert_weibo(tuple_data)  # 插入数据
-    # database.insert_redbook(last_row)  # 插入数据
+    # database.insert_tiktok(tuple_data)
+    # database.insert_weibo(tuple_data)
+    # database.insert_redbook(last_row)
